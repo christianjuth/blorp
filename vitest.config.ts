5b17b78c682b859a070f071e65adbded4c7f19c2
Christian <5768801+christianjuth@users.noreply.github.com>
2025-01-30 23:53:53 -0500

    ci: fix missing summary reporter

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,9 +4,9 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     watch: false,
-    // coverage: {
-    //   reporter: ["text", "json-summary", "json", "html"],
-    //   include: ["src/**"],
-    // },
+    coverage: {
+      reporter: ["text", "json-summary", "json", "html"],
+      include: ["src/**"],
+    },
   },
 });
