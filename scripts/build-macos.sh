7d6a9ceba71da0af7cea1a6a475c992cce38fbac
Christian <5768801+christianjuth@users.noreply.github.com>
2025-02-27 18:03:29 -0500

    refactor: update tauri icons and build script signing

--- scripts/build-macos.sh
+++ scripts/build-macos.sh
@@ -4,4 +4,8 @@ source .env
 set +a  # Stop automatic export
 
 tauri build --bundles app --target universal-apple-darwin
-xcrun productbuild --sign "$PRODUCTBUILD_SIGNING_IDENTITY" --component "./src-tauri/target/aarch64-apple-darwin/release/bundle/macos/Blorp.app" /Applications "Blorp.pkg"
+xcrun productbuild --sign "$PRODUCTBUILD_SIGNING_IDENTITY" \
+    --component "./src-tauri/target/universal-apple-darwin/release/bundle/macos/Blorp.app" /Applications \
+    Blorp.pkg
+xcrun notarytool submit Blorp.pkg --key $APPLE_API_KEY_PATH --issuer $APPLE_API_ISSUER --key-id $APPLE_API_KEY --wait
+xcrun stapler staple Blorp.pkg
