2428bec2215e5bd28fa0ab034a1c99d3ffdce7ea
Christian <5768801+christianjuth@users.noreply.github.com>
2025-04-27 00:08:42 -0400

    test: fix flaky e2e tests

--- e2e/post-feeds.spec.ts
+++ e2e/post-feeds.spec.ts
@@ -19,7 +19,9 @@ for (const { name, base } of tabs) {
         `${base}u/https%3A%2F%2Flemmy.world%2Fu%2FThe_Picard_Maneuver?type=posts`,
       );
       const postCard = page.getByTestId("post-card").first();
-      await expect(postCard).toContainText("The_Picard_Maneuver@lemmy.world");
+      await expect(postCard).toContainText("The_Picard_Maneuver@lemmy.world", {
+        ignoreCase: true,
+      });
     });
 
     test("loads search results", async ({ page }) => {
@@ -27,7 +29,9 @@ for (const { name, base } of tabs) {
       const postCard = page
         .getByTestId(base === "/communities/" ? "community-card" : "post-card")
         .first();
-      await expect(postCard).toContainText("linux");
+      await expect(postCard).toContainText("linux", {
+        ignoreCase: true,
+      });
     });
 
     test("loads community search results", async ({ page }) => {
