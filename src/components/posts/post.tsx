db6b12e3c67be6b1c5e9be56996bc119f5f7c243
Christian <5768801+christianjuth@users.noreply.github.com>
2025-05-15 19:24:42 -0400

    fix: ios image and link long press

--- src/components/posts/post.tsx
+++ src/components/posts/post.tsx
@@ -206,6 +206,7 @@ export function FeedPostCard(props: PostProps) {
       },
       filterEvents: (event) => {
         if ("button" in event) {
+          // Ignore mouse right click
           return event.button !== 2;
         }
         return true;
