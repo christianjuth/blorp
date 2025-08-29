61e597a83b3e3681fc9ce1f958d2332cae5c519f
Christian <5768801+christianjuth@users.noreply.github.com>
2025-08-29 00:15:50 -0400

    refactor: shink post screen comment mobile border size

--- src/components/comments/comment-skeleton.tsx
+++ src/components/comments/comment-skeleton.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from "../ui/skeleton";
 
 export function CommentSkeleton() {
   return (
-    <div className="max-md:mt-2 md:border-t max-md:px-3.5 py-3 flex flex-col gap-2 bg-background">
+    <div className="max-md:mt-2 border-t max-md:px-3.5 py-3 flex flex-col gap-2 bg-background">
       <div className="flex flex-row gap-2 items-center">
         <Skeleton className="h-6 w-6 rounded-full" />
         <Skeleton className="h-3 w-32" />
